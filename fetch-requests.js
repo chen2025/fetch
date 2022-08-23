b8626147a4ed0c8577cbd608812e0d9af3e9698f
Tony <yc980@georgetown.edu>
2022-08-23 17:16:01 -0400

    initial-commit

---
+++ fetch-requests.js
@@ -0,0 +1,41 @@
+/*
+Make fetch requests in the browser for each of the following tasks.
+Paste your code for fetch requests here once you finish each task.
+*/
+
+/* =============== 1. Print the status code of the response =============== */
+
+// Your code here
+fetch("/products")
+    .then(function(res) {
+        console.log(res.status);
+    });
+
+
+/* ====== 2. Print true if the status of the response was successful ====== */
+
+// Your code here
+fetch("/products")
+    .then(function(res) {
+        console.log(res.ok);
+    });
+
+
+/* =================== 3. Print the Content-Type Header =================== */
+
+// Your code here
+fetch("/products")
+    .then(function(res) {
+        console.log(res.headers.get('Content-Type'));
+    });
+
+
+/* ============== 4. Print the body of the response as text =============== */
+fetch("/products")
+  .then(function(res) {
+    return res.text();
+  })
+  .then(function(data) {
+    console.log(data);
+  });
+// Your code here
